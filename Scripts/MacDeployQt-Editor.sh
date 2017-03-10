af62e6b56fd458244bb5f1f43715c9121323b6c2
juhl <jufoot@gmail.com>
2017-03-10 12:42:16 +0900

    Fix qt app deployment

--- Scripts/MacDeployQt-Editor.sh
+++ Scripts/MacDeployQt-Editor.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd `dirname $0`
-$QT_DIR/Bin/macdeployqt ../Engine/Bin/macOS/Release/BlueshiftEditor.app -no-plugins
+$QT_DIR/Bin/macdeployqt ../Engine/Bin/macOS/Release/BlueshiftEditor.app -executable="../Engine/Bin/macOS/Release/BlueshiftEditor.app/Contents/MacOS/BlueshiftEditor" -no-plugins -always-overwrite -verbose=2
