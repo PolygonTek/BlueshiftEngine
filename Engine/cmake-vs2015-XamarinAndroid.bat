f8babed92d3fbdab4904c8886c90bf78befdfd4e
sjpark <sj6219@hotmail.com>
2017-03-14 08:57:32 +0900

    minor fix

--- Engine/cmake-vs2015-XamarinAndroid.bat
+++ Engine/cmake-vs2015-XamarinAndroid.bat
@@ -10,6 +10,8 @@ if exist "msvc2015-XamarinAndroid" rd "msvc2015-XamarinAndroid" /s /q
 md msvc2015-XamarinAndroid
 cd msvc2015-XamarinAndroid
 cmake -DCMAKE_TOOLCHAIN_FILE="CMake/Android.toolchain.cmake" -DBUILD_ENGINE=1 -DBUILD_PLAYER=1 -DANDROID=1 -DXAMARIN=1 ../..
+cd ..
+ren msvc2015-XamarinAndroid msvc2015-XamarinAndroid-temp
 @echo:
 @echo Solution created in %~dp0Build\msvc2015-XamarinAndroid
 @echo:
