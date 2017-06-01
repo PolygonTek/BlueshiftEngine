d92b12bb53249bc0679690a1a714d5a4a3669bd5
juhl <jufoot@icloud.com>
2017-06-01 14:57:24 +0900

    function name changed

--- Engine/Source/BlueshiftEngine/Private/Script/Screen/LuaModule_Screen.cpp
+++ Engine/Source/BlueshiftEngine/Private/Script/Screen/LuaModule_Screen.cpp
@@ -22,14 +22,14 @@ BE_NAMESPACE_BEGIN
 void LuaVM::RegisterScreen(LuaCpp::Module &module) {
     LuaCpp::Selector _Screen = module["Screen"];
 
-    _Screen["screenWidth"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetScreenWidth(); });
-    _Screen["screenHeight"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetScreenHeight(); });
-    _Screen["deviceWidth"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetDeviceWidth(); });
-    _Screen["deviceHeight"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetDeviceHeight(); });
-    _Screen["renderWidth"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetRenderWidth(); });
-    _Screen["renderHeight"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetRenderHeight(); });
-    _Screen["upscaleFactorX"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetUpscaleFactorX(); });
-    _Screen["upscaleFactorY"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetUpscaleFactorY(); });
+    _Screen["screen_width"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetScreenWidth(); });
+    _Screen["screen_height"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetScreenHeight(); });
+    _Screen["device_width"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetDeviceWidth(); });
+    _Screen["device_height"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetDeviceHeight(); });
+    _Screen["render_width"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetRenderWidth(); });
+    _Screen["render_height"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetRenderHeight(); });
+    _Screen["upscale_factor_x"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetUpscaleFactorX(); });
+    _Screen["upscale_factor_y"].SetFunc([]() { return renderSystem.GetMainRenderContext()->GetUpscaleFactorY(); });
 }
 
 BE_NAMESPACE_END
