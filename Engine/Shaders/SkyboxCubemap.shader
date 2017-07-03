b65bfa550d0aea6c95698196bd8e783f06c3d01d
juhl <jufoot@icloud.com>
2017-07-03 22:17:36 +0900

    vec4 to color3

--- Engine/Shaders/SkyboxCubemap.shader
+++ Engine/Shaders/SkyboxCubemap.shader
@@ -1,6 +1,6 @@
 shader "Skybox/CubeMap" {
     properties {
-        tint("Tint") : vec4 = "1 1 1 1"
+        tint("Tint") : color3 = "1 1 1"
         exposure("Exposure") : float range 0 8 0.1 = "1.0"
         rotation("Rotation") : float range 0 360 1 = "0"
         skyCubeMap("Cube Map") : object TextureAsset = "_defaultCubeTexture"
--- Engine/Source/BlueshiftEngine/Private/Render/RB_Surf_Render.cpp
+++ Engine/Source/BlueshiftEngine/Private/Render/RB_Surf_Render.cpp
@@ -66,9 +66,11 @@ void RBSurf::SetShaderProperties(const Shader *shader, const StrHashMap<Shader::
             shader->SetConstant2f(key, prop.data.As<Vec2>());
             break;
         case PropertySpec::Vec3Type:
+        case PropertySpec::Color3Type:
             shader->SetConstant3f(key, prop.data.As<Vec3>());
             break;
         case PropertySpec::Vec4Type:
+        case PropertySpec::Color4Type:
             shader->SetConstant4f(key, prop.data.As<Vec4>());
             break;
         case PropertySpec::PointType:
