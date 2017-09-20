74eb1eedff79a08fc696f305358c2afa46cd05ff
juhl <jufoot@gmail.com>
2017-09-20 13:44:36 +0900

    Replace from deprecated function system() to posix_spawn() for iOS11

--- Engine/Source/Dependencies/lua/loslib.c
+++ Engine/Source/Dependencies/lua/loslib.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <spawn.h>
 
 #define loslib_c
 #define LUA_LIB
@@ -76,10 +77,20 @@
 #endif
 
 
-
+extern char **environ;
 static int os_execute (lua_State *L) {
   const char *cmd = luaL_optstring(L, 1, NULL);
+#ifdef __APPLE__
+  pid_t pid;
+  char *argv[] = {
+    (char *)cmd,
+    NULL
+  };
+  int stat = posix_spawn(&pid, argv[0], NULL, NULL, argv, environ);
+  waitpid(pid, NULL, 0);
+#else
   int stat = system(cmd);
+#endif
   if (cmd != NULL)
     return luaL_execresult(L, stat);
   else {
