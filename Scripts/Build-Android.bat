8911c352ecc29544efd31f558ab4bc52b2d9e4cb
sjpark <sj6219@hotmail.com>
2018-02-05 02:04:41 +0900

    Android Script

--- Scripts/Build-Android.bat
+++ Scripts/Build-Android.bat
@@ -1,10 +1,36 @@
 pushd %~dp0
-cd ..\Engine
-REM call cmake-vs2015-Android.bat nopause
-call "%VS140COMNTOOLS%..\..\VC\bin\amd64\vcvars64.bat"
-REM cd Build\msvc2015-android
+
+call "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+cd ..\Engine\Project\JavaAndroid
+
+rd /S /Q AndroidPlayer\build
+rd /S /Q AndroidPlayer\app\build
+
+rd /S /Q AndroidPlayer\ARM
+rd /S /Q AndroidPlayerLibrary\ARM
+rd /S /Q BlueshiftEngine\ARM
+rd /S /Q Bullet\ARM
+rd /S /Q Etc2CompLib\ARM
+rd /S /Q etcpack_lib\ARM
+rd /S /Q freetype\ARM
+rd /S /Q HACD\ARM
+rd /S /Q jsoncpp\ARM
+rd /S /Q libjpeg\ARM
+rd /S /Q libogg\ARM
+rd /S /Q libpng\ARM
+rd /S /Q libpvrt\ARM
+rd /S /Q libvorbis\ARM
+rd /S /Q lua\ARM
+rd /S /Q LuaCpp\ARM
+rd /S /Q luasocket\ARM
+rd /S /Q minizip\ARM
+rd /S /Q nvidia\ARM
+rd /S /Q NvTriStrip\ARM
+rd /S /Q zlib\ARM
+
 REM msbuild /v:m /m BlueshiftEngine.sln /p:Configuration=Release /p:Platform=Tegra-Android
-cd Project\Android
-devenv AndroidPlayer.sln /build "Release|Tegra-Android" /project "AndroidPlayer\AndroidPlayer.vcxproj"
+devenv JavaAndroid.sln /build "Debug|ARM" /project "AndroidPlayer\AndroidPlayer.androidproj"
+devenv JavaAndroid.sln /build "Release|ARM" /project "AndroidPlayer\AndroidPlayer.androidproj"
+
 popd
 pause
