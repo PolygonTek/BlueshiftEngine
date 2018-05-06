7018b12a02fbfe20aad598a3190c8acb1288b31a
Ju-Hyung Lee <jufoot@gmail.com>
2018-05-06 10:55:38 +0900

    Minor fix

--- Shaders/SkinningMatrix1.glsl
+++ Shaders/SkinningMatrix1.glsl
@@ -1,4 +1,4 @@
-in float in_weightIndex : WEIGHT_INDEX;
+in int in_weightIndex : WEIGHT_INDEX;
 
 #ifdef VTF_SKINNING
 	#ifdef USE_BUFFER_TEXTURE
@@ -21,18 +21,18 @@ in float in_weightIndex : WEIGHT_INDEX;
 void accumulateJointMatrices(out vec4 R0, out vec4 R1, out vec4 R2, int jointIndexOffset) {
 #ifdef VTF_SKINNING
 	#ifdef USE_BUFFER_TEXTURE
-		int baseS = skinningBaseTc + (jointIndexOffset + int(in_weightIndex)) * 3;
+		int baseS = skinningBaseTc + (jointIndexOffset + in_weightIndex) * 3;
 		R0 = texelFetch(jointsMap, baseS + 0);
 		R1 = texelFetch(jointsMap, baseS + 1);
 		R2 = texelFetch(jointsMap, baseS + 2);
 	#else
-		vec2 baseST = skinningBaseTc + vec2((float(jointIndexOffset) + in_weightIndex) * 3.0, 0.0);
+		vec2 baseST = skinningBaseTc + vec2(float(jointIndexOffset + in_weightIndex) * 3.0, 0.0);
 		R0 = tex2Dlod(jointsMap, vec4((baseST + vec2(0.0, 0.0)) * invJointsMapSize, 0.0, 0.0));
 		R1 = tex2Dlod(jointsMap, vec4((baseST + vec2(1.0, 0.0)) * invJointsMapSize, 0.0, 0.0));
 		R2 = tex2Dlod(jointsMap, vec4((baseST + vec2(2.0, 0.0)) * invJointsMapSize, 0.0, 0.0));
 	#endif
 #else
-	int weightIndex = (jointIndexOffset + int(in_weightIndex)) * 3;
+	int weightIndex = (jointIndexOffset + in_weightIndex) * 3;
 	R0 = joints[weightIndex + 0];
 	R1 = joints[weightIndex + 1];
 	R2 = joints[weightIndex + 2];
