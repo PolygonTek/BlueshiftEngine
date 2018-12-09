11937adf97f97aa214b45aecd58c22ad8cc0e358
Ju-Hyung Lee <jufoot@gmail.com>
2018-12-09 09:02:11 +0900

    Remove Version.h because it will be generated by cmake

--- Source/Runtime/Version.h
+++
@@ -1,8 +0,0 @@
-// Generated automatically by cmake
-// Don't modify this file manually !
-
-#define BE_VERSION_MAJOR 0
-#define BE_VERSION_MINOR 6
-#define BE_VERSION_PATCH 0
-#define BE_VERSION_DATE "2018-12-05 12:48:18"
-
