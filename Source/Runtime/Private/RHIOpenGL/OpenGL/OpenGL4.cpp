7585ca3438e55463b7f1def5305ad7bc4cbf24b0
Ju-Hyung Lee <jufoot@gmail.com>
2020-02-12 14:41:31 +0900

    OpenGL 4.3 has ARB_ES3_compatibility that supports ETC2 compression format

--- Source/Runtime/Private/RHIOpenGL/OpenGL/OpenGL4.cpp
+++ Source/Runtime/Private/RHIOpenGL/OpenGL/OpenGL4.cpp
@@ -23,4 +23,48 @@ void OpenGL4::Init() {
     OpenGL3::Init();
 }
 
+bool OpenGL4::ImageFormatToGLFormat(Image::Format::Enum imageFormat, bool isSRGB, GLenum *glFormat, GLenum *glType, GLenum *glInternal) {
+    switch (imageFormat) {
+    case Image::Format::RGB_8_ETC2:
+        if (glFormat)   *glFormat = isSRGB ? GL_COMPRESSED_SRGB8_ETC2 : GL_COMPRESSED_RGB8_ETC2;
+        if (glType)     *glType = 0;
+        if (glInternal) *glInternal = isSRGB ? GL_COMPRESSED_SRGB8_ETC2 : GL_COMPRESSED_RGB8_ETC2;
+        return true;
+    case Image::Format::RGBA_8_1_ETC2:
+        if (glFormat)   *glFormat = isSRGB ? GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2 : GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2;
+        if (glType)     *glType = 0;
+        if (glInternal) *glInternal = isSRGB ? GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2 : GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2;
+        return true;
+    case Image::Format::RGBA_8_8_ETC2:
+        if (glFormat)   *glFormat = isSRGB ? GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC : GL_COMPRESSED_RGBA8_ETC2_EAC;
+        if (glType)     *glType = 0;
+        if (glInternal) *glInternal = isSRGB ? GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC : GL_COMPRESSED_RGBA8_ETC2_EAC;
+        return true;
+    case Image::Format::R_11_EAC:
+        if (glFormat)   *glFormat = GL_COMPRESSED_R11_EAC;
+        if (glType)     *glType = 0;
+        if (glInternal) *glInternal = GL_COMPRESSED_R11_EAC;
+        return true;
+    case Image::Format::SignedR_11_EAC:
+        if (glFormat)   *glFormat = GL_COMPRESSED_SIGNED_R11_EAC;
+        if (glType)     *glType = 0;
+        if (glInternal) *glInternal = GL_COMPRESSED_SIGNED_R11_EAC;
+        return true;
+    case Image::Format::RG_11_11_EAC:
+        if (glFormat)   *glFormat = GL_COMPRESSED_RG11_EAC;
+        if (glType)     *glType = 0;
+        if (glInternal) *glInternal = GL_COMPRESSED_RG11_EAC;
+        return true;
+    case Image::Format::SignedRG_11_11_EAC:
+        if (glFormat)   *glFormat = GL_COMPRESSED_SIGNED_RG11_EAC;
+        if (glType)     *glType = 0;
+        if (glInternal) *glInternal = GL_COMPRESSED_SIGNED_RG11_EAC;
+        return true;
+    default:
+        return OpenGL3::ImageFormatToGLFormat(imageFormat, isSRGB, glFormat, glType, glInternal);
+    }
+
+    return false;
+}
+
 BE_NAMESPACE_END
