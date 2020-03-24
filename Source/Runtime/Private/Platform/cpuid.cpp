0d01d75764174b1c4eebfde299a18440259b1c86
Ju-Hyung Lee <jufoot@gmail.com>
2020-03-24 22:32:47 +0900

    Add AVX2 / AVX512 detection

--- Source/Runtime/Private/Platform/cpuid.cpp
+++ Source/Runtime/Private/Platform/cpuid.cpp
@@ -35,130 +35,164 @@ void DetectCpu() {
     memset(&cpuInfo, 0, sizeof(cpuInfo));
 
 #if defined(__WIN32__) || (defined(__X86__) && defined(__UNIX__))
-    // __cpuid(): http://msdn.microsoft.com/en-us/library/hskdteyh(v=vs.100).aspx
-    int info[4] = { -1, };
-    __cpuid(info, 0);
-    //unsigned int nIds = info[0];
-    *((int *)(cpuInfo.vendorString)) = info[1];
-    *((int *)(cpuInfo.vendorString + 4)) = info[3];
-    *((int *)(cpuInfo.vendorString + 8)) = info[2];
+    // __cpuid() stores the supported features and CPU information in cpuinfo,
+    // an array of four 32-bit integers that is filled with the values of the EAX, EBX, ECX, and EDX registers.
+    int cpuinfo_0[4];
+    __cpuid(cpuinfo_0, 0);
+    // Maximum value for the InfoType parameter that will return valid information.
+    //unsigned int infoTypeNum = cpuinfo[0];
+    // Identification string (part 1)
+    *((int *)(cpuInfo.vendorString)) = cpuinfo_0[1];
+    // Identification string (part 2)
+    *((int *)(cpuInfo.vendorString + 4)) = cpuinfo_0[3];
+    // Identification string (part 3)
+    *((int *)(cpuInfo.vendorString + 8)) = cpuinfo_0[2];
 
-    __cpuid(info, 1);
-    cpuInfo.steppingID = info[0] & 0xf;
-    cpuInfo.model = (info[0] >> 4) & 0xf;
-    cpuInfo.family = (info[0] >> 8) & 0xf;
-    cpuInfo.processorType = (info[0] >> 12) & 0x3;
-    cpuInfo.extendedModel = (info[0] >> 16) & 0xf;
-    cpuInfo.extendedFamily = (info[0] >> 20) & 0xff;
-    cpuInfo.brandIndex = info[1] & 0xff;
-    cpuInfo.CLFLUSHCacheLineSize = ((info[1] >> 8) & 0xff) * 8;
-    cpuInfo.numLogicalProcessors = ((info[1] >> 16) & 0xff);
-    cpuInfo.APICPhysicalID = (info[1] >> 24) & 0xff;
+    int cpuinfo_1[4];
+    __cpuid(cpuinfo_1, 1);
+    cpuInfo.steppingID = cpuinfo_1[0] & 0xf;
+    cpuInfo.model = (cpuinfo_1[0] >> 4) & 0xf;
+    cpuInfo.family = (cpuinfo_1[0] >> 8) & 0xf;
+    cpuInfo.processorType = (cpuinfo_1[0] >> 12) & 0x3;
+    cpuInfo.extendedModel = (cpuinfo_1[0] >> 16) & 0xf;
+    cpuInfo.extendedFamily = (cpuinfo_1[0] >> 20) & 0xff;
+    cpuInfo.brandIndex = cpuinfo_1[1] & 0xff;
+    cpuInfo.CLFLUSHCacheLineSize = ((cpuinfo_1[1] >> 8) & 0xff) * 8;
+    cpuInfo.numLogicalProcessors = ((cpuinfo_1[1] >> 16) & 0xff);
+    cpuInfo.APICPhysicalID = (cpuinfo_1[1] >> 24) & 0xff;
 
     cpuInfo.cpuid = CPUID_NONE;
 
-    if (info[3] & BIT(23)) {
+    if (cpuinfo_1[3] & BIT(23)) {
         cpuInfo.cpuid |= CPUID_MMX;
     }
 
-    if (info[3] & BIT(15)) {
+    if (cpuinfo_1[3] & BIT(15)) {
         cpuInfo.cpuid |= CPUID_CMOV;
     }
 
-    if (info[3] & BIT(28)) {
+    if (cpuinfo_1[3] & BIT(28)) {
         cpuInfo.cpuid |= CPUID_HTT;
     }
 
-    if (info[3] & BIT(25)) {
+    if (cpuinfo_1[3] & BIT(25)) {
         cpuInfo.cpuid |= CPUID_SSE;
         cpuInfo.cpuid |= CPUID_FTZ;
 
-        if (info[3] & BIT(26)) {
+        if (cpuinfo_1[3] & BIT(26)) {
             cpuInfo.cpuid |= CPUID_SSE2;
         }
     }
 
-    if (info[2] & BIT(0)) {
+    if (cpuinfo_1[2] & BIT(0)) {
         cpuInfo.cpuid |= CPUID_SSE3;
         cpuInfo.cpuid |= CPUID_DAZ;
 
-        if (info[2] & BIT(9)) {
+        if (cpuinfo_1[2] & BIT(9)) {
             cpuInfo.cpuid |= CPUID_SSSE3;
 
-            if (info[2] & BIT(19)) {
+            if (cpuinfo_1[2] & BIT(19)) {
                 cpuInfo.cpuid |= CPUID_SSE4_1;
 
-                if (info[2] & BIT(20)) {
+                if (cpuinfo_1[2] & BIT(20)) {
                     cpuInfo.cpuid |= CPUID_SSE4_2;
+
+                    if (cpuinfo_1[2] & BIT(23)) {
+                        cpuInfo.cpuid |= CPUID_POPCNT;
+                    }
                 }
             }
         }
     }
 
-    // If Visual Studio 2010 SP1 or later
-#if (_MSC_FULL_VER >= 160040219)
     // Checking for AVX requires 3 things:
-    // 1) CPUID indicates that the OS uses XSAVE and XRSTORE
-    //     instructions (allowing saving YMM registers on context
-    //     switch)
-    // 2) CPUID indicates support for AVX
-    // 3) XGETBV indicates the AVX registers will be saved and
-    //     restored on context switch
+    // 1) CPUID indicates that the OS uses XSAVE and XRSTORE instructions (allowing saving YMM registers on context switch).
+    // 2) CPUID indicates support for AVX.
+    // 3) XGETBV indicates the AVX registers will be saved and restored on context switch.
     //
-    // Note that XGETBV is only available on 686 or later CPUs, so
-    // the instruction needs to be conditionally run.
-    bool osUsesXSAVE_XRSTORE = info[2] & BIT(27) || false;
-    bool cpuAVXSupport = info[2] & BIT(28) || false;
+    // Note that XGETBV is only available on 686 or later CPUs, so the instruction needs to be conditionally run.
+    bool osUsesXSAVE_XRSTORE = cpuinfo_1[2] & BIT(27) || false;
+    bool cpuAVXSupport = cpuinfo_1[2] & BIT(28) || false;
+    //bool cpuAVX2Support = cpuinfo_1[1] & BIT(5) || false;
 
     if (osUsesXSAVE_XRSTORE && cpuAVXSupport) {
-        // Check if the OS will save the YMM registers
+        // Check if the OS will save the YMM registers.
         unsigned long long xcrFeatureMask = _xgetbv(_XCR_XFEATURE_ENABLED_MASK);
         if (xcrFeatureMask & 0x6) {
             cpuInfo.cpuid |= CPUID_AVX;
         }
     }
-#endif
 
+    if (cpuInfo.cpuid & CPUID_AVX) {
+        int cpuinfo_7[4];
+        __cpuid(cpuinfo_7, 7);
+
+        if (cpuinfo_7[1] & BIT(5)) {
+            cpuInfo.cpuid |= CPUID_AVX2;
+
+            if (cpuinfo_7[1] & BIT(16)) {
+                cpuInfo.cpuid |= CPUID_AVX512F;
+            }
+
+            if (cpuinfo_7[1] & BIT(28)) {
+                cpuInfo.cpuid |= CPUID_AVX512CD;
+            }
+
+            if (cpuinfo_7[1] & BIT(27)) {
+                cpuInfo.cpuid |= CPUID_AVX512ER;
+            }
+
+            if (cpuinfo_7[1] & BIT(26)) {
+                cpuInfo.cpuid |= CPUID_AVX512PF;
+            }
+        }
+    }
+        
     if (!strncmp(cpuInfo.vendorString, VENDOR_INTEL, 12)) {
         cpuInfo.cpuid |= CPUID_INTEL;
 
         if (cpuInfo.family <= 4) {
-            // unsupported 386/486 processor
+            // Unsupported 386/486 processor.
             cpuInfo.cpuid |= CPUID_UNSUPPORTED;
         }
     } else if (!strncmp(cpuInfo.vendorString, VENDOR_AMD, 12)) {
         cpuInfo.cpuid |= CPUID_AMD;
 
-        __cpuid(info, 0x80000000);
-        unsigned int nExtIds = info[0];
+        int cpuinfo_ext0[4];
+        __cpuid(cpuinfo_ext0, 0x80000000);
+        unsigned int nExtIds = cpuinfo_ext0[0];
         if (nExtIds > 0) {
-            __cpuid(info, 0x80000001);
+            int cpuinfo_ext1[4];
+            __cpuid(cpuinfo_ext1, 0x80000001);
 
-            if (info[3] & BIT(31)) {
+            if (cpuinfo_ext1[3] & BIT(31)) {
                 cpuInfo.cpuid |= CPUID_3DNOW;
             }
 
-            __cpuid(info, 0x80000002);
-            *((int *)(cpuInfo.brandString)) = info[0];
-            *((int *)(cpuInfo.brandString + 4)) = info[1];
-            *((int *)(cpuInfo.brandString + 8)) = info[2];
-            *((int *)(cpuInfo.brandString + 12)) = info[3];
+            int cpuinfo_ext2[4];
+            __cpuid(cpuinfo_ext2, 0x80000002);
+            *((int *)(cpuInfo.brandString)) = cpuinfo_ext2[0];
+            *((int *)(cpuInfo.brandString + 4)) = cpuinfo_ext2[1];
+            *((int *)(cpuInfo.brandString + 8)) = cpuinfo_ext2[2];
+            *((int *)(cpuInfo.brandString + 12)) = cpuinfo_ext2[3];
 
-            __cpuid(info, 0x80000003);
-            *((int *)(cpuInfo.brandString + 16)) = info[0];
-            *((int *)(cpuInfo.brandString + 20)) = info[1];
-            *((int *)(cpuInfo.brandString + 24)) = info[2];
-            *((int *)(cpuInfo.brandString + 28)) = info[3];
+            int cpuinfo_ext3[4];
+            __cpuid(cpuinfo_ext3, 0x80000003);
+            *((int *)(cpuInfo.brandString + 16)) = cpuinfo_ext3[0];
+            *((int *)(cpuInfo.brandString + 20)) = cpuinfo_ext3[1];
+            *((int *)(cpuInfo.brandString + 24)) = cpuinfo_ext3[2];
+            *((int *)(cpuInfo.brandString + 28)) = cpuinfo_ext3[3];
 
-            __cpuid(info, 0x80000004);
-            *((int *)(cpuInfo.brandString + 32)) = info[0];
-            *((int *)(cpuInfo.brandString + 36)) = info[1];
-            *((int *)(cpuInfo.brandString + 40)) = info[2];
-            *((int *)(cpuInfo.brandString + 44)) = info[3];
+            int cpuinfo_ext4[4];
+            __cpuid(cpuinfo_ext4, 0x80000004);
+            *((int *)(cpuInfo.brandString + 32)) = cpuinfo_ext4[0];
+            *((int *)(cpuInfo.brandString + 36)) = cpuinfo_ext4[1];
+            *((int *)(cpuInfo.brandString + 40)) = cpuinfo_ext4[2];
+            *((int *)(cpuInfo.brandString + 44)) = cpuinfo_ext4[3];
         }
 
         if (cpuInfo.family <= 4) {
-            // unsupported 386/486 processor
+            // Unsupported 386/486 processor.
             cpuInfo.cpuid |= CPUID_UNSUPPORTED;
         }
     } else {
--- Source/Runtime/Public/Platform/cpuid.h
+++ Source/Runtime/Public/Platform/cpuid.h
@@ -20,27 +20,32 @@ BE_NAMESPACE_BEGIN
 // cpuid
 //--------------------------------------------------------------------------------------------------
 enum CpuId {
-    CPUID_NONE                  = 0x00000000,
-    CPUID_UNSUPPORTED           = 0x00000001,  ///< unsupported (386/486)
-    CPUID_GENERIC               = 0x00000002,  ///< unrecognized processor
-    CPUID_INTEL                 = 0x00000004,  ///< Intel
-    CPUID_AMD                   = 0x00000008,  ///< AMD
-    CPUID_ARM                   = 0x00000010,  ///< ARM
-    CPUID_HTT                   = 0x00000100,  ///< Hyper-Threading Technology
-    CPUID_CMOV                  = 0x00000200,  ///< Conditional Move (CMOV) and fast floating point comparison (FCOMI) instructions
-    CPUID_FTZ                   = 0x00000400,  ///< Flush-To-Zero mode (denormal results are flushed to zero)
-    CPUID_DAZ                   = 0x00000800,  ///< Denormals-Are-Zero mode (denormal source operands are set to zero)
-    CPUID_MMX                   = 0x00001000,  ///< Multi Media Extensions
-    CPUID_3DNOW                 = 0x00002000,  ///< 3DNow!
-    CPUID_SSE                   = 0x00004000,  ///< Streaming SIMD Extensions
-    CPUID_SSE2                  = 0x00008000,  ///< Streaming SIMD Extensions 2
-    CPUID_SSE3                  = 0x00010000,  ///< Streaming SIMD Extentions 3 aka Prescott's New Instructions
-    CPUID_SSSE3                 = 0x00020000,  ///< Supplemental Streaming SIMD Extentions 3
-    CPUID_SSE4_1                = 0x00040000,  ///< Streaming SIMD Extensions 4.1
-    CPUID_SSE4_2                = 0x00080000,  ///< Streaming SIMD Extensions 4.2
-    CPUID_AVX                   = 0x00100000,  ///< Advanced Vector Extensions
-    CPUID_AVX2                  = 0x00200000,  ///< Advanced Vector Extensions 2
-    CPUID_NEON                  = 0x00400000   ///< ARM Neon
+    CPUID_NONE                  = 0,
+    CPUID_UNSUPPORTED           = BIT(0),   ///< unsupported (386/486)
+    CPUID_GENERIC               = BIT(1),   ///< unrecognized processor
+    CPUID_INTEL                 = BIT(2),   ///< Intel
+    CPUID_AMD                   = BIT(3),   ///< AMD
+    CPUID_ARM                   = BIT(4),   ///< ARM
+    CPUID_HTT                   = BIT(5),   ///< Hyper-Threading Technology
+    CPUID_CMOV                  = BIT(6),   ///< Conditional Move (CMOV) and fast floating point comparison (FCOMI) instructions
+    CPUID_FTZ                   = BIT(7),   ///< Flush-To-Zero mode (denormal results are flushed to zero)
+    CPUID_DAZ                   = BIT(8),   ///< Denormals-Are-Zero mode (denormal source operands are set to zero)
+    CPUID_MMX                   = BIT(9),   ///< Multi Media Extensions
+    CPUID_3DNOW                 = BIT(10),  ///< 3DNow!
+    CPUID_SSE                   = BIT(11),  ///< Streaming SIMD Extensions
+    CPUID_SSE2                  = BIT(12),  ///< Streaming SIMD Extensions 2
+    CPUID_SSE3                  = BIT(13),  ///< Streaming SIMD Extentions 3 aka Prescott's New Instructions
+    CPUID_SSSE3                 = BIT(14),  ///< Supplemental Streaming SIMD Extentions 3
+    CPUID_SSE4_1                = BIT(15),  ///< Streaming SIMD Extensions 4.1
+    CPUID_SSE4_2                = BIT(16),  ///< Streaming SIMD Extensions 4.2
+    CPUID_POPCNT                = BIT(17),  ///< Population Count instruction
+    CPUID_AVX                   = BIT(18),  ///< Advanced Vector Extensions
+    CPUID_AVX2                  = BIT(19),  ///< Advanced Vector Extensions 2
+    CPUID_AVX512F               = BIT(20),  ///< AVX-512 Foundation
+    CPUID_AVX512CD              = BIT(21),  ///< AVX-512 Conflict Detection Instructions
+    CPUID_AVX512ER              = BIT(22),  ///< AVX-512 Exponential and Reciprocal Instructions
+    CPUID_AVX512PF              = BIT(23),  ///< AVX-512 Prefetch Instructions
+    CPUID_NEON                  = BIT(24)   ///< ARM Neon
 };
 
 struct CpuInfo {
