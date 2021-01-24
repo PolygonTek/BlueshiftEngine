20dc83fddf9e3a54c218f21ae725e8b456ba2cab
Ju-Hyung Lee <jufoot@gmail.com>
2021-01-24 23:14:30 +0900

    Add return_type_t

--- Source/Runtime/Precompiled.h
+++ Source/Runtime/Precompiled.h
@@ -330,6 +330,22 @@ constexpr std::size_t count_of(T (&)[N]) {
     return N;
 }
 
+template <typename T>
+struct return_type;
+template <typename R, typename... Args>
+struct return_type<R(*)(Args...)> { using type = R; };
+template <typename R, typename C, typename... Args>
+struct return_type<R(C:: *)(Args...)> { using type = R; };
+template <typename R, typename C, typename... Args>
+struct return_type<R(C:: *)(Args...) const> { using type = R; };
+template <typename R, typename C, typename... Args>
+struct return_type<R(C:: *)(Args...) volatile> { using type = R; };
+template <typename R, typename C, typename... Args>
+struct return_type<R(C:: *)(Args...) const volatile> { using type = R; };
+
+template <typename T>
+using return_type_t = typename return_type<T>::type;
+
 //----------------------------------------------------------------------------------------------
 // Win32
 //----------------------------------------------------------------------------------------------
