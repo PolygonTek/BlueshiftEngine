108857ea2be87cdb91b444e15f5df1ca0da1e99e
Ju-Hyung Lee <jufoot@gmail.com>
2022-09-30 17:55:23 +0900

    Add Physics.ray_cast_all, Physics.box_cast_all, Physics.sphere_cast_all for script API

--- Source/Runtime/Private/Scripting/Physics/LuaModule_Physics.cpp
+++ Source/Runtime/Private/Scripting/Physics/LuaModule_Physics.cpp
@@ -38,16 +38,33 @@ void LuaVM::RegisterPhysics(LuaCpp::Module &module) {
         "fraction", &CastResult::fraction,
         "surface_flags", &CastResult::surfaceFlags);
 
+    using CastResultArray = Array<CastResult>;
+
+    LuaCpp::Selector _CastResultArray = _Physics["CastResultArray"];
+    _CastResultArray.SetClass<CastResultArray>();
+    _CastResultArray.AddClassMembers<CastResultArray>(
+        "at", static_cast<CastResult & (CastResultArray:: *)(int)>(&CastResultArray::operator[]),
+        "count", &CastResultArray::Count);
+
     _Physics["gravity"].SetFunc([physicsWorld]() { return physicsWorld->GetGravity(); });
     _Physics["set_gravity"].SetFunc([physicsWorld](const Vec3 &gravity) { return physicsWorld->SetGravity(gravity); });
     _Physics["ray_cast"].SetFunc([physicsWorld](const Vec3 &start, const Vec3 &end, int mask, CastResult &castResult) {
         return physicsWorld->RayCast(start, end, mask, castResult);
     });
+    _Physics["ray_cast_all"].SetFunc([physicsWorld](const Vec3 &start, const Vec3 &end, int mask, Array<CastResult> &castResultList) {
+        return physicsWorld->RayCastAll(start, end, mask, castResultList);
+    });
     _Physics["box_cast"].SetFunc([physicsWorld](const Vec3 &boxCenter, const Vec3 &boxExtents, const Mat3 &axis, const Vec3 &end, int mask, CastResult &castResult) {
         return physicsWorld->BoxCast(boxCenter, boxExtents, axis, end, mask, castResult);
     });
+    _Physics["box_cast_all"].SetFunc([physicsWorld](const Vec3 &boxCenter, const Vec3 &boxExtents, const Mat3 &axis, const Vec3 &end, int mask, Array<CastResult> &castResultList) {
+        return physicsWorld->BoxCastAll(boxCenter, boxExtents, axis, end, mask, castResultList);
+    });
     _Physics["sphere_cast"].SetFunc([physicsWorld](const Vec3 &sphereCenter, float sphereRadius, const Vec3 &end, int mask, CastResult &castResult) {
         return physicsWorld->SphereCast(sphereCenter, sphereRadius, end, mask, castResult);
+    });
+    _Physics["sphere_cast_all"].SetFunc([physicsWorld](const Vec3 &sphereCenter, float sphereRadius, const Vec3 &end, int mask, Array<CastResult> &castResultList) {
+        return physicsWorld->SphereCastAll(sphereCenter, sphereRadius, end, mask, castResultList);
     });
 }
 
