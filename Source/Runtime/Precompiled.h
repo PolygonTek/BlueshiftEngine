6f9d1b772cc21b42c62987fa2341044248597958
Ju-Hyung Lee <jufoot@icloud.com>
2024-11-05 19:08:24 +0900

    add define DEBUG_NEW

--- Source/Runtime/Precompiled.h
+++ Source/Runtime/Precompiled.h
@@ -144,6 +144,7 @@
 #if defined(__WIN32__) && defined(_DEBUG)
 #define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
+#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
 #endif
 
 // C runtime header files.
