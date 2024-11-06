fd6b1609c001725380840d740d21c67ea12e8ff3
Ju-Hyung Lee <jufoot@icloud.com>
2024-11-06 19:15:06 +0900

    Add install-d3d12.bat

---
+++ install-d3d12.bat
@@ -0,0 +1,9 @@
+NuGet.exe install Microsoft.Direct3D.D3D12 -Source https://api.nuget.org/v3/index.json -OutputDirectory D3D12
+pause
+:: TODO
+:: copy build/native/include to Source/ThirdParty/DirectX12
+:: copy build/native/src to Source/ThirdParty/DirectX12
+:: copy build/native/bin/x64/D3D12Core.dll to Bin/Win64/Debug, Bin/Win64/Release, Bin/Win64/Development
+:: copy build/native/bin/x64/D3D12Core.pdb to Bin/Win64/Debug, Bin/Win64/Development
+:: copy build/native/bin/x64/D3D12SDKLayers.dll to Bin/Win64/Debug, Bin/Win64/Release, Bin/Win64/Development
+:: copy build/native/bin/x64/D3D12SDKLayers.pdb to Bin/Win64/Debug, Bin/Win64/Development
