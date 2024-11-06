155652e419f4e7ddbbb962565a96a627963abf1d
Ju-Hyung Lee <jufoot@icloud.com>
2024-11-06 10:35:16 +0900

    Add ClangCL cmake batch files

---
+++ cmake-vs2022clang-Win64-Editor.bat
@@ -0,0 +1,19 @@
+@echo off
+@echo:
+@echo Generating Visual Studio Solution (64 bit)
+@echo:
+pushd %~dp0
+:: remove any generated cmake build files
+if not exist "Build" md Build
+cd "Build"
+if exist "msvc2022clang" rd "msvc2022clang" /s /q
+md msvc2022clang
+cd msvc2022clang
+cmake -DBUILD_RUNTIME=1 -DBUILD_PLAYER=1 -DBUILD_EDITOR=1 -G "Visual Studio 17 2022" -A x64 -T ClangCL ..\..
+@echo:
+@echo Solution created in %~dp0Build\msvc2022clang
+@echo:
+popd
+if "%1" == "nopause" goto end
+pause
+:end
